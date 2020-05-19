7e532fb8ba4fbee5301435f5c992682c63cea07b
Felipe Fontoura <felipefontouramec@gmail.com>
2020-05-19 18:43:49 +0200

    Added and created Styles to "input"
    Added and created Styles to "input"

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
 
 <div>
-  <div class="text-center">
-    <h1>NameGator</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5 mt-5">
+        <input class="form-control mt-5 ml-5">
+      </div>
+    </div>
   </div>
 </div>
 </template>
 
 <script>
 import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
 
 export default {
   name: 'app',
@@ -17,5 +22,8 @@ export default {
 </script>
 
 <style>
-
+.row {
+  margin-top: 150px; 
+  margin-left: 200px; 
+}
 </style>
