a371043f1ca3ce15e713c22911285b35b7fa6aeb
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-05-25 13:37:17 +0200

    Created methods to add
    Created methods to add

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,13 @@ export default {
   components: {},
   data() {
     return { todos: [], todo: { checked: false}};
+  },
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+      this.todo = { checked: false};
+    }
   }
   }
 
