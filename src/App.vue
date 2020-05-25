656fe50fbc1f0fbcc81d5477aa45a0a580514c34
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-05-25 13:57:48 +0200

    Setting method remove

--- src/App.vue
+++ src/App.vue
@@ -45,12 +45,14 @@ export default {
         this.$set(this.todos, index, {...this.todos[index], checked});
       }
     },
-    removeTodo(todo) {
-      
+    removeTodo(todo) { //Ação de deletar
+      const index = this.todos.findIndex(item => item.id === todo.id);
+      if (index > -1) {
+        this.$delete(this.todos, index); 
+      }
     }
   }
-  }
-
+};
 </script>
 
 <style>
